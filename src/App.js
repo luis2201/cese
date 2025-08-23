127d0858ea444eefbdda9cc12b448780d4f2cc0a
Luis Pincay <lupin2201@gmail.com>
2025-08-23 10:26:23 -0500

    Definición de PrivateRoute

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import PrivateRoute from './components/PrivateRoute';
 import Login from './components/Login';
 
 function App() {
