8facdd78dccf945162d256083604603266d47528
Luis Pincay <lupin2201@gmail.com>
2025-08-23 10:36:46 -0500

    Creando ruta a dashboard

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import PrivateRoute from './components/PrivateRoute';
 import Login from './components/Login';
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
